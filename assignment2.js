fa5a487c6ea764355b0fad09e25c4db7ec4a4070
Masum Ahmed <masumahmed@users.noreply.github.com>
2021-06-10 08:54:12 -0400

    Implemented Functions, Methods and Some Test
    - myReduce
    - myIncludes
    - myIndexOf
    - myPush
    - myLastIndexOf
    - Object.grabKeys()
    - Object.grabValues()
    
    NOTE: FURTHER TESTING NEEDED BEFORE PULL TO MAIN BRANCH

--- assignment2.js
+++ assignment2.js
@@ -1,37 +1,36 @@
-
 //Array for testing
 
-const arr = [1,2,3];
-const arr2 = [1,,3];
+const arr = [1, 2, 3];
+const arr2 = [1, , 3];
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-const words2 = ['spray', 'limit', 'elite','', 'destruction', 'present'];
+const words2 = ['spray', 'limit', 'elite', '', 'destruction', 'present'];
 const array = [1, 2, 3, 4, 5];
 
 // FOR EACH //
-Array.prototype.myEach = function(callbackFn) {
+Array.prototype.myEach = function (callbackFn) {
     //The forEach() method executes a provided function once for each array element.//
 
     for (let i = 0; i < this.length; i++) {
-        if(this[i]== undefined) continue; //for cases like these [1,,3]
-        callbackFn(this[i],i,this);
-    }//end of for loop
+        if (this[i] == undefined) continue; //for cases like these [1,,3]
+        callbackFn(this[i], i, this);
+    } //end of for loop
 };
 
 
 //Testing myEach
 console.log("myEach: ");
-arr.myEach((x)=>console.log(x));
+arr.myEach((x) => console.log(x));
 
 console.log("forEach: ");
-arr.forEach((x)=>console.log(x));
+arr.forEach((x) => console.log(x));
 
 console.log("myEach for undefined: ");
-arr2.myEach((x)=>console.log(x));
+arr2.myEach((x) => console.log(x));
 console.log("myEach for undefined: ");
-arr2.forEach((x)=>console.log(x));
+arr2.forEach((x) => console.log(x));
 
 // MAP //
-Array.prototype.myMap = function(callbackFn) {
+Array.prototype.myMap = function (callbackFn) {
     //The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.//
     /*
         Accepts three arguments:
@@ -39,25 +38,25 @@ Array.prototype.myMap = function(callbackFn) {
             current elements index
             entire array
     */
-   const returnArray = []; //empty array that will be returned later.
-   for (let i = 0; i < this.length; i++) {
-       returnArray.push(callbackFn(this[i],i,this));
-   }//end of for loo
+    const returnArray = []; //empty array that will be returned later.
+    for (let i = 0; i < this.length; i++) {
+        returnArray.push(callbackFn(this[i], i, this));
+    } //end of for loo
 
-   return(returnArray);
+    return (returnArray);
 };
 
 
 //Testing myMap
 console.log("myMap: ");
-const map1a = arr.myMap(x=> x);
+const map1a = arr.myMap(x => x);
 console.log(map1a);
 console.log("Map: ");
-const map1b = arr.map(x=> x);
+const map1b = arr.map(x => x);
 console.log(map1b);
 
 console.log("myMap: ");
-const map2a = arr2.myMap(x=> x);
+const map2a = arr2.myMap(x => x);
 console.log(map2a);
 console.log("Map: ");
 const map2b = arr2.map(x => x);
@@ -65,7 +64,7 @@ console.log(map2b);
 
 
 // FILTER //
-Array.prototype.myFilter = function(callbackFn) {
+Array.prototype.myFilter = function (callbackFn) {
     //The filter() method creates a new array with all elements that pass the test implemented by the provided function.//
     /*
         We want to take these steps:
@@ -77,46 +76,44 @@ Array.prototype.myFilter = function(callbackFn) {
 
     const returnArray = []; //empty array
     for (let i = 0; i < this.length; i++) {
-        if (callbackFn(this[i]))
-        {
+        if (callbackFn(this[i])) {
             returnArray.push(this[i]);
-        }//end of if statement
+        } //end of if statement
 
-    }//end of for loop
-    
-    return(returnArray);
+    } //end of for loop
+
+    return (returnArray);
 };
 
 //Test myFilter
 
 const words_arr1a = words.myFilter(word => word.length > 6); //Test myFilter
 console.log("myFilter: ");
-console.log(words_arr1a);//Print myFilter
+console.log(words_arr1a); //Print myFilter
 
 const words_arr1b = words.filter(word => word.length > 6); //Test .filter
 console.log("Filter: ");
-console.log(words_arr1b);//print .filter
+console.log(words_arr1b); //print .filter
 
 
 const words_arr2a = words2.myFilter(word => word.length > 6); //Test myFilter
 console.log("myFilter: ");
-console.log(words_arr2a);//Print myFilter
+console.log(words_arr2a); //Print myFilter
 
 const words_arr2b = words2.filter(word => word.length > 6); //Test .filter
 console.log("Filter: ");
-console.log(words_arr2b);//print .filter
+console.log(words_arr2b); //print .filter
 
 // SOME //
-Array.prototype.mySome = function(callback) {
+Array.prototype.mySome = function (callback) {
     //This parameter is no longer called callbackFn according to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Some
     /*The some() method tests whether at least one element in the array passes the test implemented by the provided function. 
         It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false. 
         It doesn't modify the array.
     */
-    
+
     for (let i = 0; i < this.length; i++) {
-        if(callback(this[i]))
-        {
+        if (callback(this[i])) {
             return true;
         }
     }
@@ -126,32 +123,31 @@ Array.prototype.mySome = function(callback) {
 
 //Test mySome 
 
-const even1a = (element) => element % 2 === 0;//Test mySome
+const even1a = (element) => element % 2 === 0; //Test mySome
 console.log('mySome: ')
-console.log(array.mySome(even1a));//Print mySome
+console.log(array.mySome(even1a)); //Print mySome
 
-const even1b = (element) => element % 2 === 0;//Test Some
+const even1b = (element) => element % 2 === 0; //Test Some
 console.log('Some: ')
-console.log(array.some(even1b));//Print Some
+console.log(array.some(even1b)); //Print Some
 // expected output: true
 
-console.log('mySome: ')//Test mySome
-console.log([2, 5, 8, 1, 4].mySome(x => x > 10));  // Print and Test mySome: false 
+console.log('mySome: ') //Test mySome
+console.log([2, 5, 8, 1, 4].mySome(x => x > 10)); // Print and Test mySome: false 
 console.log('mySome: ')
 console.log([12, 5, 8, 1, 4].mySome(x => x > 10)); // Print and Test mySome: true
 
 
-console.log('Some: ')//Test Some
-console.log([2, 5, 8, 1, 4].some(x => x > 10));  // Print and Test Some: false
+console.log('Some: ') //Test Some
+console.log([2, 5, 8, 1, 4].some(x => x > 10)); // Print and Test Some: false
 console.log('Some: ')
 console.log([12, 5, 8, 1, 4].some(x => x > 10)); //Print and Test Some: true
 
 // EVERY //
-Array.prototype.myEvery = function(callbackFn) {
+Array.prototype.myEvery = function (callbackFn) {
     //The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value.//
     for (let i = 0; i < this.length; i++) {
-        if(!callbackFn(this[i]))
-        {
+        if (!callbackFn(this[i])) {
             return false;
         }
     }
@@ -163,123 +159,155 @@ const isBelowThreshold = (currentValue) => currentValue < 40;
 const sample_array = [1, 30, 39, 29, 10, 13];
 
 console.log('myEvery: ')
-console.log(sample_array.myEvery(isBelowThreshold));// Print and Test myEvery: true.
+console.log(sample_array.myEvery(isBelowThreshold)); // Print and Test myEvery: true.
 
 console.log('Every: ')
-console.log(sample_array.every(isBelowThreshold));// Print and Test Every: true.
+console.log(sample_array.every(isBelowThreshold)); // Print and Test Every: true.
 
 const isBelowThreshold2 = (currentValue) => currentValue < 30;
 console.log('myEvery: ')
-console.log(sample_array.myEvery(isBelowThreshold2));// Print and Test myEvery: false.
+console.log(sample_array.myEvery(isBelowThreshold2)); // Print and Test myEvery: false.
 
 console.log('Every: ')
-console.log(sample_array.every(isBelowThreshold2));// Print and Test Every: false.
+console.log(sample_array.every(isBelowThreshold2)); // Print and Test Every: false.
 
 function isBigEnough(element, index, array) {
     return element >= 10;
 }
-
+    
 console.log('myEvery: ')
-console.log([12, 5, 8, 130, 44].myEvery(isBigEnough));   // Print and Test myEvery: false.
+console.log([12, 5, 8, 130, 44].myEvery(isBigEnough)); // Print and Test myEvery: false.
 console.log([12, 54, 18, 130, 44].myEvery(isBigEnough)); // Print and Test myEvery: true.
 
 console.log('Every: ')
-console.log([12, 5, 8, 130, 44].every(isBigEnough));   // Print and Test Every: false.
+console.log([12, 5, 8, 130, 44].every(isBigEnough)); // Print and Test Every: false.
 console.log([12, 54, 18, 130, 44].every(isBigEnough)); // Print and Test Every: true.
 
 // REDUCE //
-Array.prototype.myReduce = function(callbackFn,initialValue) {
+Array.prototype.myReduce = function (callbackFn, initialValue) {
     //The reduce() method executes a reducer function (that you provide) on each element of the array, resulting in a single output value.//
 
-    //arguments is an Array-like object accessible inside functions that contains the values of the arguments passed to that function.
-    const initialState = arguments.length >1 ;
-    let returnValue = initialValue;
-    for (let i = 0; i < this.length; i++) {
-        if (i==0 && !initialState) 
-        {
-          returnValue = this[i];
-        }//end of if statement
+    if (initialValue === undefined)
+        accumulator = undefined;
+    else
+        accumulator = initialValue;
+
+    for (var i = 0; i < this.length; i++) {
+        if (accumulator !== undefined)
+            accumulator = callbackFn.call(undefined, accumulator, this[i], i, this);
         else
-        {
-            returnValue = callbackFn(returnValue, this[i])
-        }// end of else statement
-        
+            accumulator = this[i];
     }
-    return returnValue;
+    return accumulator;
 };
 
-//Test myReduce
-
-const sample_array2 = [1, 2, 3, 4];
+// Test Reduce
+console.log("REDUCE TESTS");
+const array1 = [1, 2, 3, 4];
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 // 1 + 2 + 3 + 4
-console.log('myReduce: ');
-console.log(sample_array2.myReduce(reducer));// Print and Test myReduce: 10 
+console.log(array1.myReduce(reducer));
 // expected output: 10
 
 // 5 + 1 + 2 + 3 + 4
-console.log('myReduce: ');
-console.log(sample_array2.myReduce(reducer, 5));// Print and Test myReduce: 15
+console.log(array1.myReduce(reducer, 5));
 // expected output: 15
-
-// 1 + 2 + 3 + 4
-console.log('Reduce: ');
-console.log(sample_array2.reduce(reducer));// Print and Test Reduce: 10 
-// expected output: 10
-
-// 5 + 1 + 2 + 3 + 4
-console.log('Reduce: ');
-console.log(sample_array2.reduce(reducer, 5));// Print and Test Reduce: 15
-// expected output: 15
-
-console.log('myReduce: ');
-let sum1 = [0, 1, 2, 3].myReduce(function (accumulator, currentValue) 
-{
-   return accumulator + currentValue//Test myReduce
-}, 0)
-console.log(sum1);//Print 6
-
-console.log('Reduce: ');
-let sum2 = [0, 1, 2, 3].reduce(function (accumulator, currentValue) 
-{
-    return accumulator + currentValue//Test Reduce: 6
-}, 0)
-console.log(sum2);//Print 6
 
 // INCLUDES //
-Array.prototype.myIncludes = function() {
+Array.prototype.myIncludes = function (searchElement, fromIndex = 0) {
     //The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.//
 
+    for (let i = fromIndex; i < this.length; i++) {
+        if (searchElement === this[i]) {
+            return true;
+        }
+    }
+    return false;
 };
 
-// INDEXOF //
-Array.prototype.myIndexOf = function() {
-    //The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.//
+// Test Includes
+let test = [1, 3, 4, 6];
+test.myIncludes(7);
+test.myIncludes(0);
+test.myIncludes(6);
 
+let test2 = ["A", "B", "Z", 5];
+
+// INDEXOF //
+Array.prototype.myIndexOf = function (searchElement, fromIndex = 0) {
+    //The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.//
+    for (let i = fromIndex; i < this.length; i++) {
+        if (searchElement === this[i]) return i;
+    }
+    return -1;
 };
 
 // PUSH //
-Array.prototype.myPush = function() {
+Array.prototype.myPush = function (elementN) {
     //The push() method adds one or more elements to the end of an array and returns the new length of the array.//
 
+    let len = this.length;
+    this[len] = elementN;
+    len++;
+    return this.length;
 };
 
+// Test Push
+arrayTest = ["Florence", "Sicily", "Rome", "Kyoto "];
+arrayTest.myPush("Miami");
+arrayTest.myPush(["New York", "Boston", "Los Angeles"]);
+console.log(arrayTest);
+console.log(arrayTest.length);
+
 // LASTINDEXOF //
-Array.prototype.myLastIndexOf = function() {
+Array.prototype.myLastIndexOf = function (searchElement, fromIndex = 0) {
     //The lastIndexOf() method returns the last index at which a given element can be found in the array, or -1 if it is not present. The array is searched backwards, starting at fromIndex.//
+
+    let lastIndexOf = -1;
+
+    for (let i = fromIndex; i < this.length; i++) {
+        if (searchElement === this[i]) lastIndexOf = i;
+    }
+
+    return lastIndexOf;
 };
 
 // KEYS //
-Object.grabKeys = function() {
+Object.grabKeys = function (obj) {
     //The Object.keys() method returns an array of a given object's own enumerable property names, iterated in the same order that a normal loop would.//
-    
+    let keys = [];
+
+    for (const property in obj) {
+        keys.push(property);
+    }
+
+    return keys;
 };
 
+// Tests Keys
+const test3 = {
+    a: "test",
+    b: "test2",
+    c: "test3"
+};
+
+console.log(Object.grabKeys(test3));
+
 // VALUES //
-Object.grabValues = function() {
+Object.grabValues = function (obj) {
     /*The Object.values() method returns an array of a given object's own enumerable property values, 
         in the same order as that provided by a for...in loop. 
         (The only difference is that a for...in loop enumerates properties in the prototype chain as well.)
-    */ 
+    */
+    let keys = [];
+
+    for (const property in obj) {
+        keys.push(obj[property]);
+    }
+
+    return keys;
 };
+
+// Test grab values
+console.log(Object.grabValues(test3));
